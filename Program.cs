f50bc63e87d9035c994e3532b90aefde1256fc73
troublemakerq <55075899+troublemakerq@users.noreply.github.com>
2020-03-20 17:54:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first1, second2;
+            Console.WriteLine("Введите первое число: ");
+
+            while (!int.TryParse(Console.ReadLine(), out first1))
+            {
+                Console.WriteLine("Введите число");
+            }
+
+            Console.WriteLine("Введите второе число: ");
+
+            while (!int.TryParse(Console.ReadLine(), out second2))
+            {
+                Console.WriteLine("Введите число");
+            }
+
+            Task(first1, second2);
+        }
+
+        public static void Task(int first1, int second2)
+        {
+            double answer = Math.Pow(first1 + second2, 2);
+            Console.WriteLine($"Ответ = : {answer}");
+        }
+    }
+}
